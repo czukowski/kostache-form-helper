c2f50427f2875f945d23fb2a0430f72741a7ede2
czukowski <carbofos@seznam.cz>
2011-04-06 09:08:56 +0200

    Removed app-specific files

--- classes/form/field/colorpicker.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Form_Field_Colorpicker extends Kohana_Form_Field_Colorpicker {}
--- classes/form/field/gender.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class Form_Field_Gender extends Kohana_Form_Field_Gender {}
